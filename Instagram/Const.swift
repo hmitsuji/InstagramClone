985048582af726e0617001977363f79dcea06274
hmitsuji <hiromunemitsuji@hiromune-no-macbook-pro.local>
2017-03-15 18:30:00 +0900

    ・課題以外の部分をいったん実装 ・画像をライブラリから選択したタイミングで止まってしまうエラーあり

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Hiromune Mitsuji on 2017/03/11.
+//  Copyright © 2017年 hmitsuji. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+  static let PostPath = "posts"
+}
---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by Hiromune Mitsuji on 2017/03/11.
+//  Copyright © 2017年 hmitsuji. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
